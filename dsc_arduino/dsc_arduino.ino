617d564eafd6c8373997751a677c18077312f4f9
Christian Kunis <ckunis98@gmail.com>
2020-07-31 21:20:00 -0400

    Began work on the UI

--- dsc_arduino/dsc_arduino.ino
+++ dsc_arduino/dsc_arduino.ino
@@ -1,33 +1,31 @@
 #include <AutoPID.h>
-#include <VariableTimedAction.h>
 
-#define SENSOR_VOLTAGE 5
-#define PWM_RESOLUTION 255.0
-double byte2volts = (SENSOR_VOLTAGE / PWM_RESOLUTION);
-
+// Feather M0 Express board pinouts
 #define REF_TEMP_PROBE_PIN A1
 #define REF_CURRENT_SENS_PIN A2
 #define SAMP_CURRENT_SENS_PIN A3
 #define SAMP_TEMP_PROBE_PIN A4
-#define Ref_Heater_PIN 10
-#define Samp_Heater_PIN 9
+#define Ref_Heater_PIN 11
+#define Samp_Heater_PIN 10
 
-//pid settings and gains
-#define OUTPUT_MIN 0
-#define OUTPUT_MAX 10
+// PID settings and gains
+#define PULSE_WIDTH 100
 #define KP 0.02
 #define KI 0.0001
 #define KD 0.002
 
-int timedActionInterval = OUTPUT_MAX;
+// Analog signal to voltage conversion factor
+#define SENSOR_VOLTAGE 5
+#define PWM_RESOLUTION 255.0
+double byte2volts = (SENSOR_VOLTAGE / PWM_RESOLUTION);
+
+// The constant voltage supplied by the output of the transformers
+#define HEATING_COIL_VOLTAGE 24
 
 // Max allowable temperature
 // If the either temperature exceeds this value, the PWM duty cycle will be set
 // set to zero
 #define MAX_TEMPERATURE 300
-
-// global variable for holding temperature and current sensor readings
-double refTemperature, sampTemperature, refCurrent, sampCurrent;
 
 // target temperature and temp control parameters
 double targetTemp = 25;
@@ -36,199 +34,165 @@ double endTemp = 30;
 double rampUpRate = 5;
 double holdTime = 0;
 
-// PID output values
-double refOutputVal, sampOutputVal;
+unsigned long startTime, latestTime, elapsedTime; // tracks clock time
+unsigned long rampUpStartTime;
+
+// global variables for holding temperature and current sensor readings
+double refTemperature, sampTemperature;
+double refCurrent, sampCurrent;
+double refHeatFlow, sampHeatFlow;
+double refPWMDutyCycle, sampPWMDutyCycle;
+
+// PID Relay output values
+bool refRelayState, sampRelayState;
 
 //input/output variables passed by reference, so they are updated automatically
-AutoPID refPID(&refTemperature, &targetTemp, &refOutputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
-AutoPID sampPID(&sampTemperature, &targetTemp, &sampOutputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
+AutoPIDRelay refPID(&refTemperature, &targetTemp, &refRelayState, PULSE_WIDTH, KP, KI, KD);
+AutoPIDRelay sampPID(&sampTemperature, &targetTemp, &sampRelayState, PULSE_WIDTH, KP, KI, KD);
 
-unsigned long startTime, latestTime, elapsedTime; // tracks clock time of sensor readings
+// The mass (in grams) of each of the material samples
+double refMass = 1, sampMass = 1;
 
-class HeaterPWM : public VariableTimedAction
+int matlabData;
+
+/*
+ * Reads the values from each of the sensor pins and converts them to the
+ * appropriate units, storing the result in the global variables
+ */
+void readSensors(double *refTemperature, double *sampTemperature, double *refCurrent, double *sampCurrent)
 {
-public:
-  HeaterPWM(int pin)
-      //sets the pin to be used for the heater
-      : pin(pin)
-  {
-    //sets the pin mode
-    pinMode(pin, OUTPUT);
-  }
+  // Read the encoded voltage signals from the sensors
+  int refTempSignal = analogRead(REF_TEMP_PROBE_PIN);
+  int sampTempSignal = analogRead(SAMP_TEMP_PROBE_PIN);
+  int refCurrentSignal = analogRead(REF_CURRENT_SENS_PIN);
+  int sampCurrentSignal = analogRead(SAMP_CURRENT_SENS_PIN);
 
-  int duration = 0;
+  // Convert the analog values into voltages
+  double refTempVoltage = refTempSignal * byte2volts;
+  double sampTempVoltage = sampTempSignal * byte2volts;
+  double refCurrentVoltage = refCurrentSignal * byte2volts;
+  double sampCurrentVoltage = sampCurrentSignal * byte2volts;
 
-private:
-  //stores the pin of this heater
-  int pin;
+  // Convert the voltage reading into appropriate units
+  *refTemperature = 0; //TODO: Put in the conversion equations
+  *sampTemperature = 0;
+  *refCurrent = 0;
+  *sampCurrent = 0;
+}
 
-  unsigned long run()
-  {
-    digitalWrite(pin, HIGH);
-    delay(duration);
-    digitalWrite(pin, LOW);
-    //delay(OUTPUT_MAX - duration);
-    // The VariableTimedAction interval will account for the time off
+////void calculateDutyCycle(double* refPWMDutyCycle, double* sampPWMDutyCycle, )
 
-    //returns the amount in seconds to wait before executing the next event
-    //if 0 is returned, then the previous interval is maintained
-    return 0;
-  }
-};
-
-//The HeaterPWM objects that control the heaters
-HeaterPWM refHeaterPWM(Ref_Heater_PIN);
-HeaterPWM sampHeaterPWM(Samp_Heater_PIN);
-
-//Temperature controller class is responsible for reading the inputs from the sensors and updating the duty cycle of the heaters
-class TemperatureController : public VariableTimedAction
+void calculateHeatFlow(double *refHeatFlow, double *sampHeatFlow, double refCurrent, double sampCurrent)
 {
-public:
-  TemperatureController()
-  {
-    // Set the temperature and current sensor pins
-    pinMode(REF_TEMP_PROBE_PIN, INPUT);
-    pinMode(SAMP_TEMP_PROBE_PIN, INPUT);
-    pinMode(REF_CURRENT_SENS_PIN, INPUT);
-    pinMode(SAMP_CURRENT_SENS_PIN, INPUT);
+  *refHeatFlow = refCurrent * HEATING_COIL_VOLTAGE / refMass;
+  *sampHeatFlow = sampCurrent * HEATING_COIL_VOLTAGE / sampMass;
+}
 
-    //if temperature is more than 4 degrees below or above setpoint, OUTPUT will be set to min or max respectively
-    refPID.setBangBang(4);
-    sampPID.setBangBang(4);
-    //set PID update interval to 4000ms
-    refPID.setTimeStep(4000);
-    sampPID.setTimeStep(4000);
+void updateTargetTemperature(double *targetTemp, double startTemp, double endTemp, double rampUpRate, double latestTime)
+{
+  if (((*targetTemp < startTemp) && (startTemp < endTemp)) ||
+      ((*targetTemp > startTemp) && (startTemp > endTemp)))
+  {
+    *targetTemp = startTemp;
+    rampUpStartTime = millis();
   }
-
-private:
-  unsigned long run()
+  else if (((*targetTemp < endTemp) && (endTemp > startTemp)) ||
+           ((*targetTemp > endTemp) && (endTemp < startTemp)))
   {
+    *targetTemp = startTemp + (rampUpRate / 60) * (latestTime - rampUpStartTime);
+  }
+  else
+  {
+    *targetTemp = endTemp;
+  }
+}
+
+void sendData()
+{
+  Serial.print("Elapsed time (ms): ");
+  Serial.println(elapsedTime);
+
+  Serial.print("Target Temperature (C): ");
+  Serial.println(targetTemp);
+
+  Serial.print("Ref Temperature (C): ");
+  Serial.println(refTemperature);
+
+  Serial.print("Samp Temperature (C): ");
+  Serial.println(sampTemperature);
+
+  Serial.print("Ref Current (A): ");
+  Serial.println(refCurrent);
+
+  Serial.print("Ref Current (A): ");
+  Serial.println(sampCurrent);
+
+  Serial.print("Ref PWM Duty Cycle: ");
+  Serial.println(refPID.getPulseValue() / PULSE_WIDTH);
+
+  Serial.print("Samp PWM Duty Cycle: ");
+  Serial.println(sampPID.getPulseValue() / PULSE_WIDTH);
+}
+
+void controlLoop()
+{
+  digitalWrite(13, HIGH);
+  startTime = millis();
+  while (true)
+  {
+    digitalWrite(13, HIGH);
+    // Check for interupts from the UI
+    if (Serial.available())
+    {
+      // read the incoming data as a string
+      matlabData = Serial.read();
+      Serial.print("Recieved data: ");
+      Serial.println(matlabData);
+
+      switch (matlabData)
+      {
+      case 5:
+        return;
+        break;
+      default:
+        break;
+      }
+    }
+
     // Record the time
     latestTime = millis();
     elapsedTime = latestTime - startTime;
 
     // Read the measurements from the sensors
-    readSensors();
+    readSensors(&refTemperature, &sampTemperature, &refCurrent, &sampCurrent);
 
-    // Calculate the error between the latest temperatures and the target temperature
-
-    // Caclutate the heat flow
+    // Calcutate the heat flow
+    calculateHeatFlow(&refHeatFlow, &sampHeatFlow, refCurrent, sampCurrent);
 
     // Calculate the new target temperature
-    updateTargetTemperature();
+    updateTargetTemperature(&targetTemp, startTemp, endTemp, rampUpRate, latestTime);
 
     // Run the PID algorithm
-    if (refTemperature < MAX_TEMPERATURE)
-    {
-      refPID.run();
-    }
-    else
-    {
-      refOutputVal = 0;
-    }
-    if (sampTemperature < MAX_TEMPERATURE)
-    {
-      sampPID.run();
-    }
-    else
-    {
-      sampOutputVal = 0;
-    }
+    refPID.run();
+    sampPID.run();
 
-    // Update the PWM
-    refHeaterPWM.duration = 10 * refOutputVal;
-    sampHeaterPWM.duration = 10 * sampOutputVal;
-    /*
-    analogWrite(Ref_Heater_PIN, refOutputVal);
-    analogWrite(Samp_Heater_PIN, sampOutputVal);
-    */
+    // Update the PWM Relay output
+    if (refTemperature < MAX_TEMPERATURE)
+      digitalWrite(Ref_Heater_PIN, refRelayState);
+    else
+      digitalWrite(Ref_Heater_PIN, LOW);
+
+    if (sampTemperature < MAX_TEMPERATURE)
+      digitalWrite(Samp_Heater_PIN, sampRelayState);
+    else
+      digitalWrite(Samp_Heater_PIN, LOW);
 
     // Send data out via Serial bus
     sendData();
 
-    return 0;
-  }
-
-  /*
-   * Reads the values from each of the sensor pins and converts them to the
-   * appropriate units, storing the result in the global variables
-   */
-  void readSensors()
-  {
-    // Read the encoded voltage signals from the sensors
-    int refTempVal = analogRead(REF_TEMP_PROBE_PIN);
-    int sampTempVal = analogRead(SAMP_TEMP_PROBE_PIN);
-    int refCurrentVal = analogRead(REF_CURRENT_SENS_PIN);
-    int sampCurrentVal = analogRead(SAMP_CURRENT_SENS_PIN);
-
-    // Convert the analog values into voltages
-    double refTempVoltage = refTempVal * byte2volts;
-    double sampTempVoltage = sampTempVal * byte2volts;
-    double refCurrentVoltage = refCurrentVal * byte2volts;
-    double sampCurrentVoltage = sampCurrentVal * byte2volts;
-
-    // Convert the voltage reading into appropriate units
-    refTemperature = 0; //TODO: Put in the conversion equations
-  }
-
-  void updateTargetTemperature()
-  {
-    if (((endTemp > startTemp) && (targetTemp < endTemp)) || ((endTemp < startTemp) && (targetTemp > endTemp)))
-    {
-      targetTemp = startTemp + (rampUpRate / 60) * (latestTime - startTime);
-    }
-    else
-    {
-      targetTemp = endTemp;
-    }
-  }
-
-  void sendData()
-  {
-    Serial.print("elapsed time (ms)");
-    Serial.println(elapsedTime);
-
-    Serial.print("ref_tmp");
-    Serial.println(refTemperature);
-
-    Serial.print("samp_tmp");
-    Serial.println(refTemperature);
-
-    Serial.print("ref_pwm");
-    Serial.println(refOutputVal);
-
-    Serial.print("samp_pwm");
-    Serial.println(refOutputVal);
-  }
-};
-
-TemperatureController temperatureController;
-
-void controlLoop()
-{
-  digitalWrite(13, HIGH);
-
-  refHeaterPWM.start(timedActionInterval);
-  sampHeaterPWM.start(timedActionInterval);
-  temperatureController.start(0.1 * timedActionInterval);
-
-  while (true)
-  {
-    // Check for interupts from the UI
-    if (Serial.available())
-    {
-      Serial.println(Serial.read());
-    }
-
-    VariableTimedAction::updateActions();
-
     //TODO
     // Check loop exit conditions
   }
-
-  refHeaterPWM.stop();
-  sampHeaterPWM.stop();
-  temperatureController.stop();
 }
 
 void setup()
@@ -236,6 +200,23 @@ void setup()
   Serial.begin(9600);
 
   pinMode(13, OUTPUT);
+
+  // Set the temperature and current sensor pins
+  pinMode(REF_TEMP_PROBE_PIN, INPUT);
+  pinMode(SAMP_TEMP_PROBE_PIN, INPUT);
+  pinMode(REF_CURRENT_SENS_PIN, INPUT);
+  pinMode(SAMP_CURRENT_SENS_PIN, INPUT);
+
+  // Set the relay output pins
+  pinMode(Ref_Heater_PIN, OUTPUT);
+  pinMode(Samp_Heater_PIN, OUTPUT);
+
+  //if temperature is more than 4 degrees below or above setpoint, OUTPUT will be set to min or max respectively
+  refPID.setBangBang(4);
+  sampPID.setBangBang(4);
+  //set PID update interval to 1000ms
+  refPID.setTimeStep(1000);
+  sampPID.setTimeStep(1000);
 }
 
 void loop()
@@ -247,9 +228,20 @@ void loop()
 
   if (Serial.available())
   {
-    Serial.println(Serial.read());
+    digitalWrite(13, HIGH);
+    
+    // read the incoming data as a string
+    matlabData = Serial.read();
+    Serial.print("Recieved data: ");
+    Serial.println(matlabData);
 
-    //switch case when start command, call the control loop function
-    //switch Serial.read();
+    switch (matlabData)
+    {
+    case 3:
+      //controlLoop;
+      break;
+    default:
+      break;
+    }
   }
 }
