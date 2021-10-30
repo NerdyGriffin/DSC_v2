814d028c66fec82e5ad1057f7de4d191da6e2d6f
Christian Kunis <ckunis98@gmail.com>
2021-10-30 15:55:33 -0400

    Update indent size

--- dsc_octave_gui/fcn/loadConfigFile.m
+++ dsc_octave_gui/fcn/loadConfigFile.m
@@ -1,123 +1,123 @@
-w %-*- texinfo -*-
+%-*- texinfo -*-
 %@deftypefn {Function File} {@var{ret} =}  loadConfigFile()
 %Function loadConfigFile()
 %@end deftypefn
 
 function configLoadStatus = loadConfigFile (dlg)
-    %   Load control parameters from a .ini file
+  %   Load control parameters from a .ini file
 
-    ini = IniConfig();
+  ini = IniConfig();
 
-    % Prompt the user to select a file
-    [configFileName, configFilePath] = uigetfile('*.ini');
+  % Prompt the user to select a file
+  [configFileName, configFilePath] = uigetfile('*.ini');
 
-    % Re-focus the dlg window
-    drawnow;
-    figure(dlg.UIFigure)
+  % Re-focus the dlg window
+  drawnow;
+  figure(dlg.UIFigure)
 
-    switch configFileName
-        case 0
-            % Cancel the read operation and return an empty array
-            % if the user closes the file selection window
-            configLoadStatus = false;
-            return
+  switch configFileName
+    case 0
+      % Cancel the read operation and return an empty array
+      % if the user closes the file selection window
+      configLoadStatus = false;
+      return
 
-        otherwise
-            % Create and display the progress bar
-            updateProgressDlg(dlg, 'Loading config file...');
-            dlg.SharedProgressDlg.Title = 'Loading Config';
+    otherwise
+      % Create and display the progress bar
+      updateProgressDlg(dlg, 'Loading config file...');
+      dlg.SharedProgressDlg.Title = 'Loading Config';
 
-            % Create fully-formed filename as a string
-            configFullPath = fullfile(configFilePath, configFileName);
+      % Create fully-formed filename as a string
+      configFullPath = fullfile(configFilePath, configFileName);
 
-            % Read the .ini file
-            ini.ReadFile(configFullPath);
+      % Read the .ini file
+      ini.ReadFile(configFullPath);
 
-            PIDSection = 'PID Settings';
+      PIDSection = 'PID Settings';
 
-            if ini.IsSections(PIDSection)
+      if ini.IsSections(PIDSection)
 
-                if ini.IsKeys(PIDSection, 'Kp')
-                    dlg.Data.Kp = ...
-                        ini.GetValues('PID Settings', 'Kp');
-                end
+        if ini.IsKeys(PIDSection, 'Kp')
+          dlg.Data.Kp = ...
+            ini.GetValues('PID Settings', 'Kp');
+        end
 
-                if ini.IsKeys(PIDSection, 'Ki')
-                    dlg.Data.Ki = ...
-                        ini.GetValues('PID Settings', 'Ki');
-                end
+        if ini.IsKeys(PIDSection, 'Ki')
+          dlg.Data.Ki = ...
+            ini.GetValues('PID Settings', 'Ki');
+        end
 
-                if ini.IsKeys(PIDSection, 'Kd')
-                    dlg.Data.Kd = ...
-                        ini.GetValues('PID Settings', 'Kd');
-                end
+        if ini.IsKeys(PIDSection, 'Kd')
+          dlg.Data.Kd = ...
+            ini.GetValues('PID Settings', 'Kd');
+        end
 
-            else
-                % Close the progress bar
-                if isvalid(dlg.SharedProgressDlg)
-                    close(dlg.SharedProgressDlg);
-                end
+      else
+        % Close the progress bar
+        if isvalid(dlg.SharedProgressDlg)
+          close(dlg.SharedProgressDlg);
+        end
 
-                warningMessage = sprintf("The selected .ini file does not contain a [%s] section", PIDSection);
-                uialert(dlg.UIFigure, warningMessage, 'Invalid File');
-                configLoadStatus = false;
-                return
-            end
+        warningMessage = sprintf("The selected .ini file does not contain a [%s] section", PIDSection);
+        uialert(dlg.UIFigure, warningMessage, 'Invalid File');
+        configLoadStatus = false;
+        return
+      end
 
-            TempControlSection = 'Temperature Control';
+      TempControlSection = 'Temperature Control';
 
-            if ini.IsSections(TempControlSection)
+      if ini.IsSections(TempControlSection)
 
-                if ini.IsKeys(TempControlSection, 'startTemp')
-                    dlg.Data.startTemp = ...
-                        ini.GetValues(TempControlSection, 'startTemp');
-                    dlg.StartTempCEditField.Value = dlg.Data.startTemp;
-                end
+        if ini.IsKeys(TempControlSection, 'startTemp')
+          dlg.Data.startTemp = ...
+            ini.GetValues(TempControlSection, 'startTemp');
+          dlg.StartTempCEditField.Value = dlg.Data.startTemp;
+        end
 
-                if ini.IsKeys(TempControlSection, 'endTemp')
-                    dlg.Data.endTemp = ...
-                        ini.GetValues(TempControlSection, 'endTemp');
-                    dlg.EndTempCEditField.Value = dlg.Data.endTemp;
-                end
+        if ini.IsKeys(TempControlSection, 'endTemp')
+          dlg.Data.endTemp = ...
+            ini.GetValues(TempControlSection, 'endTemp');
+          dlg.EndTempCEditField.Value = dlg.Data.endTemp;
+        end
 
-                if ini.IsKeys(TempControlSection, 'rampUpRate')
-                    dlg.Data.rampUpRate = ...
-                        ini.GetValues(TempControlSection, 'rampUpRate');
-                    dlg.RateCminEditField.Value = dlg.Data.rampUpRate;
-                end
+        if ini.IsKeys(TempControlSection, 'rampUpRate')
+          dlg.Data.rampUpRate = ...
+            ini.GetValues(TempControlSection, 'rampUpRate');
+          dlg.RateCminEditField.Value = dlg.Data.rampUpRate;
+        end
 
-                if ini.IsKeys(TempControlSection, 'holdTime')
-                    dlg.Data.holdTime = ...
-                        ini.GetValues(TempControlSection, 'holdTime');
-                    dlg.HoldTimesecEditField.Value = dlg.Data.holdTime;
-                end
+        if ini.IsKeys(TempControlSection, 'holdTime')
+          dlg.Data.holdTime = ...
+            ini.GetValues(TempControlSection, 'holdTime');
+          dlg.HoldTimesecEditField.Value = dlg.Data.holdTime;
+        end
 
-            else
-                % Close the progress bar
-                if isvalid(dlg.SharedProgressDlg)
-                    close(dlg.SharedProgressDlg);
-                end
+      else
+        % Close the progress bar
+        if isvalid(dlg.SharedProgressDlg)
+          close(dlg.SharedProgressDlg);
+        end
 
-                warningMessage = sprintf("The selected .ini file does not contain a [%s] section", TempControlSection);
-                uialert(dlg.UIFigure, warningMessage, 'Invalid File');
-                configLoadStatus = false;
-                return
-            end
+        warningMessage = sprintf("The selected .ini file does not contain a [%s] section", TempControlSection);
+        uialert(dlg.UIFigure, warningMessage, 'Invalid File');
+        configLoadStatus = false;
+        return
+      end
 
-    end
+  end
 
-    % Create and display the progress bar
-    updateProgressDlg(dlg, 'Awaiting response from Arduino...');
+  % Create and display the progress bar
+  updateProgressDlg(dlg, 'Awaiting response from Arduino...');
 
-    sendPIDGains(dlg);
-    receivePIDGains(dlg);
+  sendPIDGains(dlg);
+  receivePIDGains(dlg);
 
-    sendControlParameters(dlg);
-    receiveControlParameters(dlg);
+  sendControlParameters(dlg);
+  receiveControlParameters(dlg);
 
-    % Close the progress bar
-    if isvalid(dlg.SharedProgressDlg)
-        close(dlg.SharedProgressDlg);
-    end
+  % Close the progress bar
+  if isvalid(dlg.SharedProgressDlg)
+    close(dlg.SharedProgressDlg);
+  end
 
 endfunction
--- dsc_octave_gui/fcn/refreshLivePlot.m
+++ dsc_octave_gui/fcn/refreshLivePlot.m
@@ -4,20 +4,20 @@
 %@end deftypefn
 
 function ret = refreshLivePlot (dlg, elapsedTimeArray, ...
-        targetTempArray, refTempArray, sampTempArray)
+    targetTempArray, refTempArray, sampTempArray)
 
-    if (init)
-        elapsedTimeArray = [0, 1];
-        targetTempArray = [0, 0];
-        refTempArray = [0, 1];
-        sampTempArray = [0, -1];
-    endif
+  if (init)
+    elapsedTimeArray = [0, 1];
+    targetTempArray = [0, 0];
+    refTempArray = [0, 1];
+    sampTempArray = [0, -1];
+  endif
 
-    %plot (dlg.Main_Plot, x, y, "k:"); %, x, y, "b", x, y, "r");
-    plot (dlg.Main_Plot, elapsedTimeArray, targetTempArray, "k:",
-    elapsedTimeArray, refTempArray, "b",
-    elapsedTimeArray, sampTempArray, "r");
-    legend(dlg.Main_Plot, 'Target Temperature', 'Reference Sample', 'Test Sample', 'Location', 'best');
+  %plot (dlg.Main_Plot, x, y, "k:"); %, x, y, "b", x, y, "r");
+  plot (dlg.Main_Plot, elapsedTimeArray, targetTempArray, "k:",
+  elapsedTimeArray, refTempArray, "b",
+  elapsedTimeArray, sampTempArray, "r");
+  legend(dlg.Main_Plot, 'Target Temperature', 'Reference Sample', 'Test Sample', 'Location', 'best');
 
-    ret = 0;
+  ret = 0;
 endfunction
