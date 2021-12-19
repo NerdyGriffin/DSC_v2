e765349335ae0b58ccf8e8c59ec6b9b9716fae9c
Christian Kunis <ckunis98@gmail.com>
2021-12-19 10:41:42 -0500

    Update current_sensor_test to use I2C sensors

--- debug/current_sensor_test/current_sensor_test.ino
+++ debug/current_sensor_test/current_sensor_test.ino
@@ -19,6 +19,8 @@
  */
 
 #include <Adafruit_NeoPixel.h>
+#include <INA219_WE.h>
+#include <Wire.h>
 
 // NeoPixel parameters
 #define LED_PIN 8
@@ -37,11 +39,15 @@ const uint32_t blue = neopixel.Color(0, 0, 255);
 
 // Feather M0 Express board pinouts
 #define REF_TEMP_PROBE_PIN A1
-#define REF_CURRENT_SENS_PIN A2
-#define SAMP_CURRENT_SENS_PIN A3
 #define SAMP_TEMP_PROBE_PIN A4
 #define Ref_Heater_PIN 11
 #define Samp_Heater_PIN 10
+#define REF_CURRENT_I2C 0x41
+#define SAMP_CURRENT_I2C 0x40
+
+// INA219 Current Sensor Devices
+INA219_WE REF_INA219 = INA219_WE(REF_CURRENT_I2C);   // ref heater ina219 board
+INA219_WE SAMP_INA219 = INA219_WE(SAMP_CURRENT_I2C); // sample heater ina219 board
 
 /**
  * Number of samples to average the reading over. Change this to make the
@@ -58,10 +64,10 @@ const uint32_t blue = neopixel.Color(0, 0, 255);
 
 // Wait 2 milliseconds before the next loop for the analog-to-digital converter
 // to settle after the last reading
-#define AVG_SAMPLE_DELAY 2 // Sample delay in milliseconds
+#define AVG_SAMPLE_DELAY 2000UL // Sample delay in microseconds
 
 // global variable for holding the raw analog sensor values
-unsigned long sensorValues[4];
+unsigned long sensorValues[2];
 
 // The max voltage of analog input readings
 #define ANALOG_REF_VOLTAGE 3.3
@@ -76,29 +82,12 @@ const double byteToMillivolts = 1000.0 * byteToVolts;
 #define AMPLIFIER_VOLTAGE_OFFSET 1250.0 // 1250 mV = 1.25 V
 #define AMPLIFIER_CONVERSION_FACTOR 5.0 // 5 mV/C = 0.005 V/C
 
-// Current sensor conversion constants
-#define CURRENT_SENSOR_SENS 0.4 // Sensitivity (Sens) 100mA per 250mV = 0.4
-// #define CURRENT_SENSOR_VREF 1650.0 // Output voltage with no current: ~ 1650mV or 1.65V
-#define CURRENT_SENSOR_VREF 0.0 // VRef = {Output voltate with no current} - ANALOG_REF/2
-// VREF is now accounted for by `sensorValue - analogMidpoint` during the RMS
-// calculation
-double Ref_Current_Sensor_Sens = 0.4, Samp_Current_Sensor_Sens = 0.4;
-double Ref_Current_Sensor_VRef = 0.0, Samp_Current_Sensor_VRef = 0.0;
-
-// The constant voltage supplied to the heating coils
-#define HEATING_COIL_VOLTAGE 24.0 // Theoretical 24 VAC
-// I recommend measuring the real-world voltage across the resistor and
-// adjusting this value accordingly
-
-// The resistance of the heating coil circuit (Ohms, not kilo-Ohms)
-#define HEATING_COIL_RESISTANCE 49.0
-// For best accuracy during sensor calibration, this value should be measured as
-// the total resistance around the heating coil circuit, not just the resistance
-// of the heating components alone.
+#define REF_TEMP_CALIBRATION_OFFSET 6.666666666666666
+#define SAMP_TEMP_CALIBRATION_OFFSET 3.000000000000000
 
 // Max allowable temperature. If the either temperature exceeds this value, the
 // PWM duty cycle will be set set to zero
-#define MAX_TEMPERATURE 300
+#define MAX_TEMPERATURE 200.0
 
 // target temperature and temp control parameters
 double targetTemp;
@@ -107,7 +96,14 @@ unsigned long elapsedTime; // tracks clock time
 
 // global variables for holding temperature and current sensor readings
 double refTemperature, sampTemperature;
+
+double refShuntVoltage_mV, sampShuntVoltage_mV;
+double refBusVoltage_V, sampBusVoltage_V;
+double refLoadVoltage_V, sampLoadVoltage_V;
 double refCurrent_mA, sampCurrent_mA;
+double refPower, sampPower;
+bool refOverflow = false, sampOverflow = false;
+
 double refHeatFlow, sampHeatFlow;
 
 // The mass (in grams) of each of the material samples
@@ -117,41 +113,30 @@ double refMass = 1, sampMass = 1;
  * Reads the values from each of the sensor pins and computes to average of
  * multiple measurements for each pin
  */
-void readSensorValues()
+void readTempSensorValues()
 {
-  unsigned long latestSampleTime;
-
   // Zero the array before taking samples
   memset(sensorValues, 0, sizeof(sensorValues));
 
   // Read the analog signals from the sensors
+  unsigned long latestSampleTime = micros();
   for (int i = 0; i < AVG_SAMPLES; i++)
   {
-    latestSampleTime = millis();
-
     sensorValues[0] += analogRead(REF_TEMP_PROBE_PIN);
     sensorValues[1] += analogRead(SAMP_TEMP_PROBE_PIN);
 
-    // Current sensor values are squared for RMS calculation
-    sensorValues[2] += sq(analogRead(REF_CURRENT_SENS_PIN) - analogMidpoint);
-    sensorValues[3] += sq(analogRead(SAMP_CURRENT_SENS_PIN) - analogMidpoint);
-
     // Wait 2 milliseconds before the next loop for the analog-to-digital
     // converter to settle after the last reading
-    while ((millis() - latestSampleTime) < AVG_SAMPLE_DELAY)
-      ;
+    while ((micros() - latestSampleTime) < AVG_SAMPLE_DELAY)
+      ; // busy wait
+
+    latestSampleTime += AVG_SAMPLE_DELAY;
   }
 
   // Calculate the average of the samples
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 2; i++)
   {
     sensorValues[i] = sensorValues[i] / AVG_SAMPLES;
-  }
-
-  // Calculate the RMS for current sensors
-  for (int i = 2; i < 4; i++)
-  {
-    sensorValues[i] = sqrt(sensorValues[i]);
   }
 }
 
@@ -161,22 +146,32 @@ void readSensorValues()
  */
 void updateSensorData()
 {
-  readSensorValues();
+  readTempSensorValues();
 
   // The voltage is in millivolts
   double refTempVoltage = sensorValues[0] * byteToMillivolts;
   double sampTempVoltage = sensorValues[1] * byteToMillivolts;
-  double refCurrentVoltage = sensorValues[2] * byteToMillivolts;
-  double sampCurrentVoltage = sensorValues[3] * byteToMillivolts;
 
   // Convert the voltage readings into appropriate units. This will calculate
-  // the temperature (in Celcius)
-  refTemperature = (refTempVoltage - AMPLIFIER_VOLTAGE_OFFSET) / AMPLIFIER_CONVERSION_FACTOR;
-  sampTemperature = (sampTempVoltage - AMPLIFIER_VOLTAGE_OFFSET) / AMPLIFIER_CONVERSION_FACTOR;
-  // This will calculate the actual current (in mA). Using the ~~Vref~~ and
-  // sensitivity settings you configure
-  refCurrent_mA = (refCurrentVoltage - CURRENT_SENSOR_VREF) * CURRENT_SENSOR_SENS;
-  sampCurrent_mA = (sampCurrentVoltage - CURRENT_SENSOR_VREF) * CURRENT_SENSOR_SENS;
+  // the temperature (in Celsius)
+  refTemperature = (refTempVoltage - AMPLIFIER_VOLTAGE_OFFSET) / AMPLIFIER_CONVERSION_FACTOR - REF_TEMP_CALIBRATION_OFFSET;
+  sampTemperature = (sampTempVoltage - AMPLIFIER_VOLTAGE_OFFSET) / AMPLIFIER_CONVERSION_FACTOR - SAMP_TEMP_CALIBRATION_OFFSET;
+
+  REF_INA219.startSingleMeasurement();
+  refShuntVoltage_mV = REF_INA219.getShuntVoltage_mV();
+  refBusVoltage_V = REF_INA219.getBusVoltage_V();
+  refCurrent_mA = REF_INA219.getCurrent_mA();
+  refPower = REF_INA219.getBusPower();
+  refLoadVoltage_V = refBusVoltage_V + (refShuntVoltage_mV / 1000);
+  refOverflow = REF_INA219.getOverflow();
+
+  SAMP_INA219.startSingleMeasurement();
+  sampShuntVoltage_mV = SAMP_INA219.getShuntVoltage_mV();
+  sampBusVoltage_V = SAMP_INA219.getBusVoltage_V();
+  sampCurrent_mA = SAMP_INA219.getCurrent_mA();
+  sampPower = SAMP_INA219.getBusPower();
+  sampLoadVoltage_V = sampBusVoltage_V + (sampShuntVoltage_mV / 1000);
+  sampOverflow = SAMP_INA219.getOverflow();
 }
 
 /**
@@ -184,12 +179,13 @@ void updateSensorData()
  */
 void calculateHeatFlow()
 {
-  // Convert current from milliAmps to Amps
-  double refCurrentAmps = refCurrent_mA / 1000.0;
-  double sampCurrentAmps = sampCurrent_mA / 1000.0;
-  // Calculate the heat flow as milliWatts per gram
-  refHeatFlow = refCurrent_mA * HEATING_COIL_VOLTAGE / refMass;
-  sampHeatFlow = refCurrent_mA * HEATING_COIL_VOLTAGE / sampMass;
+  // // Convert current from milliAmps to Amps
+  // double refCurrentAmps = refCurrent_mA / 1000.0;
+  // double sampCurrentAmps = sampCurrent_mA / 1000.0;
+
+  // Calculate the heat flow as Watts per gram
+  refHeatFlow = (refPower / 1000.0) / refMass;
+  sampHeatFlow = (sampPower / 1000.0) / sampMass;
 }
 
 /**
@@ -197,10 +193,7 @@ void calculateHeatFlow()
  */
 void sendData()
 {
-  // Send the char 'd' to indicate the start of data set
-  Serial.println('d');
-
-  Serial.println("ElapsedTime(ms),TargetTemp(C),RefTemp(C),SampTemp(C),RefCurrent(mA),SampCurrent(mA),RefHeatFlow(),SampHeatFlow(),RefDutyCycle(%),SampDutyCycle(%)");
+  Serial.println("ElapsedTime(ms),TargetTemp(C),RefTemp(C),SampTemp(C),RefShuntVoltage(mV),SampShuntVoltage(mV),RefBusVoltage(V),SampBusVoltage(V),RefLoadVoltage(V),SampLoadVoltage(V),RefCurrent(mA),SampCurrent(mA),RefBusPower(mW),SampBusPower(mW),RefHeatFlow(),SampHeatFlow(),RefDutyCycle(%),SampDutyCycle(%)");
 
   // Send each value in the expected order, separated by commas
   Serial.print(elapsedTime);
@@ -213,9 +206,29 @@ void sendData()
   Serial.print(sampTemperature);
   Serial.print(",");
 
+  Serial.print(refShuntVoltage_mV);
+  Serial.print(",");
+  Serial.print(sampShuntVoltage_mV);
+  Serial.print(",");
+
+  Serial.print(refBusVoltage_V);
+  Serial.print(",");
+  Serial.print(sampBusVoltage_V);
+  Serial.print(",");
+
+  Serial.print(refLoadVoltage_V);
+  Serial.print(",");
+  Serial.print(sampLoadVoltage_V);
+  Serial.print(",");
+
   Serial.print(refCurrent_mA);
   Serial.print(",");
   Serial.print(sampCurrent_mA);
+  Serial.print(",");
+
+  Serial.print(refPower);
+  Serial.print(",");
+  Serial.print(sampPower);
   Serial.print(",");
 
   Serial.print(refHeatFlow);
@@ -231,16 +244,47 @@ void sendData()
 void setup()
 {
   Serial.begin(9600);
+  while (!Serial)
+  {
+    // will pause Zero, Leonardo, etc until serial console opens
+    delay(1);
+  }
+
+  // Initialize INA219 boards.
+  Wire.begin();
+  if (!REF_INA219.init())
+  {
+    Serial.println("Failed to find INA219 chip for reference heater");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  if (!SAMP_INA219.init())
+  {
+    Serial.println("Failed to find INA219 chip for sample heater");
+    while (1)
+    {
+      delay(10);
+    }
+  }
 
   pinMode(13, OUTPUT);
 
   analogReadResolution(ANALOG_RESOLUTION);
 
+  // Set up ADC mode for INA219 boards
+  // ADC automatic hw averaging is available with SAMPLE_MODE_XX, using single sample here
+  REF_INA219.setADCMode(BIT_MODE_12);
+  SAMP_INA219.setADCMode(BIT_MODE_12);
+
+  // Set measurement mode to single-shot triggered
+  REF_INA219.setMeasureMode(TRIGGERED);
+  SAMP_INA219.setMeasureMode(TRIGGERED);
+
   // Set the temperature and current sensor pins
   pinMode(REF_TEMP_PROBE_PIN, INPUT);
   pinMode(SAMP_TEMP_PROBE_PIN, INPUT);
-  pinMode(REF_CURRENT_SENS_PIN, INPUT);
-  pinMode(SAMP_CURRENT_SENS_PIN, INPUT);
 
   // Set the relay output pins
   pinMode(Ref_Heater_PIN, OUTPUT);
@@ -268,6 +312,9 @@ void loop()
 
   // Set placeholder target temp
   targetTemp = 300;
+
+  // Send data out via Serial bus
+  sendData();
 
   digitalWrite(13, LOW); // Blink the LED
 
@@ -309,7 +356,4 @@ void loop()
     neopixel.show();
     break;
   }
-
-  // Send data out via Serial bus
-  sendData();
 }
